a4d7ab9d96f4715d19efa28152f6e5c67cbd5940
NataliaBebebino <nbebebino@gmail.com>
2022-08-09 15:32:38 -0400

    Replace useState with useRef in AddUser component

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,22 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import Card from "../UI/Card";
 import classes from "./AddUser.module.css";
 import Button from "../UI/Button";
 import ErrorModal from "../UI/ErrorModal";
 
 const AddUser = (props) => {
-  const [enteredUsername, setEnteredUsername] = useState("");
-  const [enteredAge, setEnteredAge] = useState("");
   const [error, setError] = useState();
-  /*
-  In case you don't want to use array destructuring: 
-  const ageUseState = useState("");
-  const enteredAge = ageUseState[0];
-  const setEnteredAge = ageUseState[1];
-  */
+
+  const nameInputRef = useRef();
+  const ageInputRef = useRef();
 
   const addUserHandler = (event) => {
     event.preventDefault();
+
+    const enteredUsername = nameInputRef.current.value;
+    const enteredAge = ageInputRef.current.value;
 
     // Here I make sure that both fields of the form are filled out
     if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
@@ -38,43 +36,33 @@ const AddUser = (props) => {
 
     props.onAddUser(enteredUsername, enteredAge); // with this line of code we forward the data to the App component upon every click.
 
-    setEnteredUsername("");
-    setEnteredAge("");
-  };
-
-  const usernameChangeHandler = (event) => {
-    setEnteredUsername(event.target.value);
-  };
-
-  const ageChangeHandler = (event) => {
-    setEnteredAge(event.target.value);
+    //We should not manipulate the DOM without using React (if we are working with React). Anyway, in this case we are just resetting the state, it makes no harm...
+    nameInputRef.current.value = "";
+    ageInputRef.current.value = "";
   };
 
   const errorHandler = () => {
     setError(null); // the idea is to set error to null/ undefined, so that it becomes a falsy value and then the ErrorModal is not displayed.
-  }
+  };
 
   return (
     <div>
-      {error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler}/>} {/*Remember that in this &&, if the first argument is truthy, then it returns the second argument. */}
+      {error && (
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          onConfirm={errorHandler}
+        />
+      )}{" "}
+      {/*Remember that in this &&, if the first argument is truthy, then it returns the second argument. */}
       <Card className={classes.input}>
         {/* This className here is not coming from the usual attributes of the HTML tags because Card is a customized tag, it's our tag where we defined the className props */}
         <form onSubmit={addUserHandler}>
           <label htmlFor="username">Username</label>
-          <input
-            id="username"
-            type="text"
-            value={enteredUsername}
-            onChange={usernameChangeHandler}
-          />
+          <input id="username" type="text" ref={nameInputRef} />
 
           <label htmlFor="age">Age (Years)</label>
-          <input
-            id="age"
-            type="number"
-            value={enteredAge}
-            onChange={ageChangeHandler}
-          />
+          <input id="age" type="number" ref={ageInputRef} />
 
           <Button type="submit">Add User</Button>
         </form>
